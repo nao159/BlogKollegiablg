61a9ac4384a94836673e6c64326bba18bcf9b421
Naomikka <79338207+nao159@users.noreply.github.com>
2021-09-15 20:21:28 +0300

    update requirements with prycopg2

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def admin_permissions(func):
 Base = declarative_base()
 app = Flask(__name__)
 SECRET_KEY = os.urandom(32)
-app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY_NEW')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -46,7 +46,7 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blogs.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
